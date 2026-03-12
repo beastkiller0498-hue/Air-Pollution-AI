3c592eb20686ddcb3f4c83abec14728bc7f0d815
beastkiller0498-hue <beastkiller0498@gmail.com>
2026-03-12 10:37:35 -0800

    Update script.js

--- air_pollution_website/static/script.js
+++ air_pollution_website/static/script.js
@@ -1,36 +1,58 @@
-const ctx = document.getElementById("pollutionChart")
+/* MAP */
 
-new Chart(ctx,{
+var lat = 17.0
+var lon = 79.5
 
-type:"bar",
+var map = L.map('map').setView([lat, lon], 5)
 
-data:{
-
-labels:["PM2.5","PM10","NO2","SO2","O3"],
-
-datasets:[{
-
-label:"Pollution Levels",
-
-data:[pm25,pm10,no2,so2,o3],
-
-backgroundColor:[
-"#ef4444",
-"#f97316",
-"#3b82f6",
-"#22c55e",
-"#6366f1"
-]
-
-}]
-
-}
-
-})
-
-
-var map = L.map('map').setView([20,78],4)
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+/* Base map */
+L.tileLayer(
+"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+{
 maxZoom:18
 }).addTo(map)
+
+
+/* AQI Heatmap from OpenWeather */
+L.tileLayer(
+"https://tile.openweathermap.org/map/aqi/{z}/{x}/{y}.png?appid=YOUR_API_KEY",
+{
+opacity:0.6
+}).addTo(map)
+
+
+/* Marker */
+var marker = L.marker([lat, lon]).addTo(map)
+
+marker.bindPopup(`
+<b>${city}</b><br>
+AQI: ${aqi} - ${status}
+`).openPopup()
+
+
+/* -------- AQI LEGEND -------- */
+
+var legend = L.control({position: "bottomright"})
+
+legend.onAdd = function () {
+
+var div = L.DomUtil.create("div", "legend")
+
+div.innerHTML += "<h4>AQI Levels</h4>"
+
+div.innerHTML +=
+'<i style="background:#00e400"></i> Good <br>'
+
+div.innerHTML +=
+'<i style="background:#ffff00"></i> Fair <br>'
+
+div.innerHTML +=
+'<i style="background:#ff7e00"></i> Poor <br>'
+
+div.innerHTML +=
+'<i style="background:#ff0000"></i> Severe'
+
+return div
+}
+
+legend.addTo(map)
