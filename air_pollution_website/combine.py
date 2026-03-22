308772c0f26b7129eba39c54803a83d47668e26c
beastkiller0498-hue <beastkiller0498@gmail.com>
2026-03-22 10:58:17 -0800

    Update combine.py

--- air_pollution_website/combine.py
+++ air_pollution_website/combine.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+# Load all city files
+df1 = pd.read_csv("delhi.csv")
+df2 = pd.read_csv("hyderabad.csv")
+df3 = pd.read_csv("mumbai.csv")
+df4 = pd.read_csv("kolkata.csv")
+df5 = pd.read_csv("visakhapatnam.csv")
+
+# Combine all
+final_df = pd.concat([df1, df2, df3, df4, df5], ignore_index=True)
+
+# Save new dataset
+final_df.to_csv("final_dataset.csv", index=False)
+
+print("Final dataset created successfully!")
